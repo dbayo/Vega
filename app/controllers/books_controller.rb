a5ef14aa9c33c847fb131737d60e729698e55922
dbayo <david.bayo.nieto@gmail.com>
2014-05-14 21:43:41 +0200

    Empezar con el buscador de la enciclopedia (2)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
 	def index
+		params[:genre] = "all" if params[:genre].blank?
 		@cards = Card.search(params[:search]).getGenre(params[:genre])
 	end
 end
