245ac4691d7adeca5dc3728d41c6787381f22e2a
dbayo <david.bayo.nieto@gmail.com>
2014-05-14 18:26:21 +0200

    Primera subida Vega

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vega::Application.config.secret_key_base = 'f7817f5db9d2233ab41a31a855f70bbff1b783340cedc725efc52029ff9318dde831f6f11aabd1ed6758f44541188bf5d2367d4d70a55a970628596d1182ea7e'
