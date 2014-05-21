0899b7e55d987794b6f7439d556de048fd9ba9e5
dbayo <david.bayo.nieto@gmail.com>
2014-05-21 19:05:24 +0200

    Admin controller

---
+++ app/controllers/educators_controller.rb
@@ -0,0 +1,20 @@
+class EducatorsController < ApplicationController
+	before_filter :setNavSidebar
+	def plan
+		@news = News.where(:active => true, :language => I18n.locale)
+		@teacherComments = TeacherComment.where(:active => true, :language => I18n.locale)
+		@totalPictures = Attachment.count
+		@totalComments = Comment.count
+		@totalUsers = User.count
+		@totalNewUsers = User.where("created_at > ?", 1.month.ago).count
+	end
+
+	def teach
+
+	end
+
+	private
+	def setNavSidebar
+		@navSidebar = "educators"
+	end
+end
